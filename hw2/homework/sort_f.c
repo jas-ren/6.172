9c9bc50a788fe3251b409886e28437833fa149f1
jas-ren <>
2025-03-03 16:10:31 -0500

    allocate memory for array copy only once

--- hw2/homework/sort_f.c
+++ hw2/homework/sort_f.c
@@ -23,7 +23,72 @@
 
 #include "./util.h"
 
-void sort_f(data_t* A, int p, int r) {
-  printf("Unimplemented!\n");
+
+// Function prototypes
+static void merge_f(data_t* A, data_t* left, int p, int q, int r);
+static void copy_f(data_t* source, data_t* dest, int n);
+void sort_f(data_t* A, int p, int r);
+void isort(data_t* left, data_t* right);
+void sort_f_recursive(data_t* A, data_t* left, int p, int r);
+
+// A basic merge sort routine that sorts the subarray A[p..r]
+inline void sort_f(data_t* A, int p, int r) {
+  assert(A);
+  data_t* left = 0;
+  mem_alloc(&left, (r - p) / 2 + 1);
+  sort_f_recursive(A, left, p, r);
+  mem_free(&left);
+}
+
+
+inline void sort_f_recursive(data_t* A, data_t* left, int p, int r) {
+  if (r - p < 10) {
+    isort(A + p, A + r);
+  } else {
+    int q = (p + r) / 2;
+    sort_f_recursive(A, left, p, q);
+    sort_f_recursive(A, left, q + 1, r);
+    merge_f(A, left, p, q, r);
+  }
+}
+
+// A merge routine. Merges the sub-arrays A [p..q] and A [q + 1..r].
+// Uses two arrays 'left' and 'right' in the merge operation.
+inline static void merge_f(data_t* A, data_t* left, int p, int q, int r) {
+  assert(A);
+  assert(p <= q);
+  assert((q + 1) <= r);
+  int n1 = q - p + 1;
+
+  copy_f(&(A[p]), left, n1);
+  left[n1] = UINT_MAX;
+
+  data_t* i = left;
+  data_t* A_p = A + p;
+  data_t* A_q = A + q + 1;
+  data_t* const A_end = A + r + 1;
+
+  // Then our loop
+  while (A_p < A_end && A_q < A_end) {
+      if (*A_q <= *i) {
+          *A_p++ = *A_q++;
+      } else {
+          *A_p++ = *i++;
+      }
+  }
+  while (*i < UINT_MAX) {
+    *A_p++ = *i++;
+  }
+
+}
+
+inline static void copy_f(data_t* source, data_t* dest, int n) {
+  assert(dest);
+  assert(source);
+
+  const data_t* end = dest + n;
+  while (dest < end) {
+    *dest++ = *source++;
+  }
 }
 
