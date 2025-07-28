f65933600f76e3b7d825f8e395ed0efe3442cf42
jas-ren <78553107+jas-ren@users.noreply.github.com>
2025-07-28 02:09:10 +0000

    remove printing out entire bitarray

--- MIT6_172F18-project1/everybit/tests.c
+++ MIT6_172F18-project1/everybit/tests.c
@@ -178,8 +178,7 @@ void testutil_frmstr(const char* const bitstring) {
     current_bit = boolfromchar(bitstring[i]);
     bitarray_set(test_bitarray, i, current_bit);
   }
-  // bitarray_fprint(stdout, test_bitarray);
-  bitarray_pprint(test_bitarray);
+
   if (test_verbose) {
     fprintf(stdout, " newstr lit=%s\n", bitstring);
     testutil_expect(bitstring);
